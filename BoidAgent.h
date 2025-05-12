f443a30bf2d1d49a0f7871ac9648c17643c8ec6f
victor cesario <victor.cesario@hotmail.com>
2025-05-12 13:23:28 -0300

    render bounding box

--- BoidAgent.h
+++ BoidAgent.h
@@ -10,7 +10,40 @@
 
 namespace BoidAgent
 {
-	float vertices[] = {
+	const float BOX_WIDTH = 2.0f;
+	const float BOX_HEIGHT = 4.0f;
+	const float BOX_LENGTH = 3.0f;
+
+	float m_BoxVertices[] = {
+		-1.0f, -1.0f, -1.0f, // 0 - left bottom back
+		 1.0f, -1.0f, -1.0f, // 1 - right bottom back
+		 1.0f,  1.0f, -1.0f, // 2 - right top back
+		-1.0f,  1.0f, -1.0f, // 3 - left top back
+		-1.0f, -1.0f,  1.0f, // 4 - left bottom front
+		 1.0f, -1.0f,  1.0f, // 5 - right bottom front
+		 1.0f,  1.0f,  1.0f, // 6 - right top front
+		-1.0f,  1.0f,  1.0f  // 7 - left top front
+	};
+
+	unsigned int m_BoxIndices[] = {
+		// back face
+		0, 1,
+		1, 2,
+		2, 3,
+		3, 0,
+
+		4, 5,
+		5, 6,
+		6, 7,
+		7, 4,
+
+		0, 4,
+		1, 5,
+		2, 6,
+		3, 7,
+	};
+
+	float m_AgentVertices[] = {
 		// Tip of the cone
 		 0.0f, 1.0f,  0.0f,   // Vertex 0: tip
 
@@ -21,7 +54,7 @@ namespace BoidAgent
 		 -0.5f, 0.0f,  0.5f    // Vertex 4: front-left
 	};
 
-	unsigned int indices[] = {
+	unsigned int m_AgentIndices[] = {
 		// Side triangles (tip to base)
 		0, 1, 2,   // Tip to back-left and back-right
 		0, 2, 3,   // Tip to back-right and front-right
@@ -33,8 +66,9 @@ namespace BoidAgent
 		1, 3, 4    // Base triangle 2
 	};
 
-	unsigned int vao;
-	std::unique_ptr<Shader> shader;
+	unsigned int m_AgentVao;
+	unsigned int m_BoxVao;
+	std::unique_ptr<Shader> m_AgentShader, m_BoxShader;
 	float m_ScrWidth, m_ScrHeight;
 
 	void Init(int screenWidth, int screenHeight)
@@ -42,52 +76,90 @@ namespace BoidAgent
 		m_ScrWidth = screenWidth;
 		m_ScrHeight = screenHeight;
 
-		unsigned int vbo;
-		unsigned int ebo;
+		unsigned int agentVbo;
+		unsigned int agentEbo;
 
-		glGenVertexArrays(1, &vao);
-		glGenBuffers(1, &vbo);
-		glGenBuffers(1, &ebo);
+		glGenVertexArrays(1, &m_AgentVao);
+		glGenBuffers(1, &agentVbo);
+		glGenBuffers(1, &agentEbo);
 
-		glBindVertexArray(vao);
+		glBindVertexArray(m_AgentVao);
 
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, agentVbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(m_AgentVertices), m_AgentVertices, GL_STATIC_DRAW);
 
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, agentEbo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_AgentIndices), m_AgentIndices, GL_STATIC_DRAW);
 
 		// position attribute
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
 
-		shader = std::make_unique<Shader>("AgentShader.vert", "AgentShader.frag");
-		
-		shader->use();
+		m_AgentShader = std::make_unique<Shader>("AgentShader.vert", "AgentShader.frag");
 
-		shader->setMat4("model", glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f)));
+		m_AgentShader->use();
+
+		m_AgentShader->setMat4("model", glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f)));
 		//shader->setMat4("view", glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f)));
 		//shader->setMat4("projection", glm::perspective(glm::radians(45.0f), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f));
+
+		unsigned int boxVbo;
+		unsigned int boxEbo;
+
+		glGenVertexArrays(1, &m_BoxVao);
+		glGenBuffers(1, &boxVbo);
+		glGenBuffers(1, &boxEbo);
+
+		glBindVertexArray(m_BoxVao);
+
+		glBindBuffer(GL_ARRAY_BUFFER, boxVbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(m_BoxVertices), m_BoxVertices, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, boxEbo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_BoxIndices), m_BoxIndices, GL_STATIC_DRAW);
+
+		// position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		m_BoxShader = std::make_unique<Shader>("BoxShader.vert", "BoxShader.frag");
+		//m_BoxShader->use();
+
 	}
 
 	void Render(Camera camera)
 	{
-		shader->use();
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), m_ScrWidth / m_ScrHeight, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
 
-		shader->setMat4("projection", glm::perspective(glm::radians(camera.Zoom), m_ScrWidth / m_ScrHeight, 0.1f, 100.0f));
-		shader->setMat4("view", camera.GetViewMatrix());
+		// agents
+		glBindVertexArray(m_AgentVao);
 
-		glBindVertexArray(vao);
+		m_AgentShader->use();
+		m_AgentShader->setMat4("projection", projection);
+		m_AgentShader->setMat4("view", view);
 
-		glm::mat4 baseRotation = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		glm::mat4 agentRotScale = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-		shader->setMat4("model", baseRotation);
+		m_AgentShader->setMat4("model", agentRotScale);
 		glDrawElements(GL_TRIANGLES, 6 * 3, GL_UNSIGNED_INT, 0);
 
-		shader->setMat4("model", glm::translate(baseRotation, glm::vec3(4, 0, 0)));
+		m_AgentShader->setMat4("model", glm::translate(agentRotScale, glm::vec3(4, 0, 0)));
 		glDrawElements(GL_TRIANGLES, 6 * 3, GL_UNSIGNED_INT, 0);
 
-		shader->setMat4("model", glm::translate(baseRotation, glm::vec3(0, 2, 0)));
+		m_AgentShader->setMat4("model", glm::translate(agentRotScale, glm::vec3(0, 2, 0)));
 		glDrawElements(GL_TRIANGLES, 6 * 3, GL_UNSIGNED_INT, 0);
+
+		// bounding box
+		glBindVertexArray(m_BoxVao);
+
+		m_BoxShader->use();
+		glm::mat4 boxRotScale = glm::rotate(glm::scale(glm::mat4(1.0f), glm::vec3(BOX_WIDTH, BOX_HEIGHT, BOX_LENGTH)),
+											glm::radians(0.0f), glm::vec3(1, 0, 0));
+		m_BoxShader->setMat4("model", boxRotScale);
+		m_BoxShader->setMat4("projection", projection);
+		m_BoxShader->setMat4("view", view);
+
+		glDrawElements(GL_LINES, 12 * 2, GL_UNSIGNED_INT, 0);
 	}
 }
